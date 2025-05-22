79ad6efe6cc68f6cbbcd525c589c666bc18d40ca
amirmnoohi <amirmnoohi@gmail.com>
2025-05-22 18:05:59 +0000

    Add QEMU Code Base

---
+++ build/meson-private/__CMake_compiler_info__/CMakeFiles/3.22.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.15.158")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.15.158")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "aarch64")
+
+include("/home/amirmnoohi/huawei/128bit-on-demand/qemu-10.0.0/build/meson-private/__CMake_compiler_info__/CMakeMesonTempToolchainFile.cmake")
+
+set(CMAKE_SYSTEM "Linux-5.15.158")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "5.15.158")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_CROSSCOMPILING "FALSE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ build/tests/tcg/aarch64-softmmu/config-cc.mak
@@ -0,0 +1,2 @@
+CROSS_CC_HAS_ARMV8_3=-march=armv8.3-a
+CROSS_CC_HAS_ARMV8_MTE=-march=armv8.5-a+memtag
